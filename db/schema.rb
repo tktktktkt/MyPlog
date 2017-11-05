16fb1e879d5104ea93673b966b4ec0c77d817c6e
Tekeshi <thinkmore115@gmail.com>
2017-11-05 00:14:08 +0000

    lesson-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104131625) do
+ActiveRecord::Schema.define(version: 20171105000441) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171104131625) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "weight"
   end
 
 end
